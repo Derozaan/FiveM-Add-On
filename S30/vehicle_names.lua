24f8cb354aed7e9a65fb353e7a386f17351bb20e
Dein Marez <deinmarez@gmail.com>
2019-04-03 00:18:01 +0700

    S30

---
+++ S30/vehicle_names.lua
@@ -0,0 +1,8 @@
+Citizen.CreateThread(function()
+	--   Nissan Fairlady Z
+	AddTextEntry('0xD7F77EA1', 'Nissan Fairlady Z')
+	AddTextEntry('0x38F9BD5E', 'Devil Z')
+        AddTextEntry('0xF20778CC', 'Devil Z plate')
+        AddTextEntry('0x0F02781A', 'Exhaust')
+        
+end)
