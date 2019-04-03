4959293907775bbad6473526347724a69dfcbea3
Dein Marez <deinmarez@gmail.com>
2019-04-04 04:45:20 +0700

    regera

---
+++ regera/vehicle_names.lua
@@ -0,0 +1,12 @@
+Citizen.CreateThread(function()
+	--   Chiron
+	AddTextEntry('0x406DC176', 'Audi')
+	AddTextEntry('0x7DF88D29', 'RS6')
+        AddTextEntry('0x8B90FECF', 'ABT Exhaust Chrome')
+        AddTextEntry('0x19CD9B4A', 'Akrapovic Exhaust')
+        AddTextEntry('0x39CBA8AC', 'ABT Rear Bumper')
+        AddTextEntry('0x7549D241', 'ABT Exhaust Black')
+        AddTextEntry('0xB3EF1750', 'ABT Front Bumper')
+        AddTextEntry('0xBA9C5E3A', 'ABT Spoiler')
+        
+      end)
