0fde8fde6da6f84559b6600d8dafad220a52d086
Dein Marez <deinmarez@gmail.com>
2019-04-04 00:36:01 +0700

    ae86

---
+++ ae86/vehicle_names.lua
@@ -0,0 +1,13 @@
+Citizen.CreateThread(function()
+	--   Fenyr Supersport
+	AddTextEntry('0x4E6DC57B', '2005 FORD GT')
+	AddTextEntry('0x7D5E59A1', 'GT40 MK.1 Style Spoiler')
+        AddTextEntry('0x0161CF13', 'GT40 MK.1 Style Mirror')
+        AddTextEntry('0x7FD5DE40', 'GT3 Spoiler')
+        AddTextEntry('0x22C2F447', 'GT3 Diffuser')
+        AddTextEntry('0xA08D2243', 'GT3 Hood')
+        AddTextEntry('0xC4E1271D', 'GT3 Cage')
+        AddTextEntry('0xCDBC5E17', 'Forza Racing')
+        AddTextEntry('0xE101306D', 'Grid')
+        AddTextEntry('0xF76FB17D', 'GT40 MK.1 Style')
+end)
