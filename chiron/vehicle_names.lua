b99950e966b271ca0f0c9390b0c1fd944c540d26
Dein Marez <deinmarez@gmail.com>
2019-04-04 00:00:57 +0700

    AddOn

---
+++ chiron/vehicle_names.lua
@@ -0,0 +1,18 @@
+Citizen.CreateThread(function()
+	--   Chiron
+	AddTextEntry('0x64B43298', 'Chiron')
+	AddTextEntry('0x1F739AA7', 'Lexus')
+        AddTextEntry('0x6D0B15CA', 'McLaren')
+        AddTextEntry('0x70A9FE89', 'Aventador')
+        AddTextEntry('0x268D09BB', 'Pagani')
+        AddTextEntry('0x648B3BAA', 'MP4')
+        AddTextEntry('0x51146FF3', 'Viper SRT10')
+        AddTextEntry('0xA9ED9703', 'Bugatti')
+        AddTextEntry('0xAC6B8829', 'Lamborghini')
+        AddTextEntry('0xB472B915', 'GS 350')
+        AddTextEntry('0xC7D5F5FB', 'Reventon')
+        AddTextEntry('0xCDDF250E', 'Veyron')
+        AddTextEntry('0xE8522014', 'Zonda Tricolore')
+        AddTextEntry('0xFC3882B4', 'Dodge')
+
+end)
---
+++ sesto/vehicle_names.lua
@@ -0,0 +1,4 @@
+Citizen.CreateThread(function()
+	--   Sesto Elemento
+	
+  end)
