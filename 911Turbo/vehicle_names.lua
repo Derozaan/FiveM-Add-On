746c39c8d59adc7645ca31731ceb42a675307049
Dein Marez <deinmarez@gmail.com>
2019-04-03 22:22:51 +0700

    911 Turbo

---
+++ 911Turbo/vehicle_names.lua
@@ -0,0 +1,6 @@
+Citizen.CreateThread(function()
+	--   Porsche 911 Turbo S 2016
+	AddTextEntry('0x5A6B90E1', 'Porsche 911 Turbo S 2016')
+	AddTextEntry('0x79FC371E', 'Porsche 911 Turbo S 2016')
+        
+end)
