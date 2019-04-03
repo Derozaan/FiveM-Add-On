0f67a4c6c0f6e2473db332c2abc456cf597af34e
Dein Marez <deinmarez@gmail.com>
2019-04-04 04:09:02 +0700

    aha

--- R32/vehicle_names.lua
+++
@@ -1,8 +0,0 @@
-Citizen.CreateThread(function()
-	--   Nissan Skyline GT-R(BNR32)
-	AddTextEntry('0x8C34D838', 'Nissan Skyline GT-R(BNR32)')
-	AddTextEntry('0x2B97DAFA', 'Nismo')
-        AddTextEntry('0x3B04E41A', 'Spoiler')
-        AddTextEntry('0x2FCED87B', 'Exhaust')
-        
-end)
