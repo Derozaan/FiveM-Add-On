4f4f7356d6d9e8cc7e2c399d57dcf6a1a83a1e51
Dein Marez <deinmarez@gmail.com>
2019-04-04 20:09:15 +0700

    Racecar

---
+++ r91cp/vehicle_names.lua
@@ -0,0 +1,6 @@
+Citizen.CreateThread(function()
+	--   Nissan 91CP
+	AddTextEntry('0x004D4C50', 'Nissan')
+	AddTextEntry('0xE0838CD1', 'R91CP 1991')
+        
+end)
---
+++ sauberc9/vehicle_names.lua
@@ -0,0 +1,6 @@
+Citizen.CreateThread(function()
+	--   Sauber C9
+	AddTextEntry('0x422F6B6F', 'Mercedes-Benz')
+	AddTextEntry('0xCC33CB74', 'Sauber C9 #63 1989')
+        
+end)
