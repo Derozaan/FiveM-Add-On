813f80f5c914f3959901b09a43dfe54251a80404
Dein Marez <deinmarez@gmail.com>
2019-04-04 21:15:00 +0700

    Panamera

---
+++ Panamera/vehicle_names.lua
@@ -0,0 +1,6 @@
+Citizen.CreateThread(function()
+	--   Porsche Panamera Turbo '17
+	AddTextEntry('0x0B95CC5E', 'Porsche Panamera Turbo '17')
+	AddTextEntry('0x48426C5C', 'Porsche Panamera Turbo '17')
+        
+end)
