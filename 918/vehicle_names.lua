6fdbb99cc21b81d45d9677ff2a358fcec9d70a50
Dein Marez <deinmarez@gmail.com>
2019-04-06 04:32:55 +0700

    918

---
+++ 918/vehicle_names.lua
@@ -0,0 +1,16 @@
+Citizen.CreateThread(function()
+	--   Porsche 911 Turbo S 2016
+	AddTextEntry('0x2F3202EC', '918 Spyder')
+	AddTextEntry('0xBF49FF01', 'Porsche')
+        AddTextEntry('0x3E4C94EC', 'installed on the original color roof')
+        AddTextEntry('0x5A30992A', 'Weissach lightweight kit')
+        AddTextEntry('0x9B056CA4', 'Racing type carbon fiber tail')
+        AddTextEntry('0x162EABA2', 'Super lightweight component')
+        AddTextEntry('0x1940E91D', 'Fixed raised carbon fiber tail')
+        AddTextEntry('0x4385064E', 'leather luxury components')
+        AddTextEntry('0x95488508', 'Open the engine hatch cover')
+        AddTextEntry('0xBF9A35CD', 'Fixed rising primary color tail')
+        AddTextEntry('0xCF34B068', 'Racing front brackets')
+        AddTextEntry('0xD377AC71', 'Install the lightweight roof')
+
+end)
