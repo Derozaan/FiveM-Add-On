597a020c461367e4c501e9d4b5ba8ef0aa421039
Dein Marez <deinmarez@gmail.com>
2019-05-03 08:57:29 +0700

    Test

---
+++ city/vehicle_names.lua
@@ -0,0 +1,11 @@
+Citizen.CreateThread(function()
+	--   City Turbo
+	AddTextEntry('0xC21B40CA','City Turbo II '85')
+	AddTextEntry('0x02DD72C8','Motocompo '83')
+        AddTextEntry('0x4B57D2F6','Glossy Carbon Fibre Hood')
+        AddTextEntry('0x8EA93900','Mugen Power Steering Wheel')
+        AddTextEntry('0x631F22C7','Honda')
+        AddTextEntry('0xC7F53A90','Recaro Seats')
+        AddTextEntry('0xC21B40CA','City Turbo II '85')
+        AddTextEntry('0xF8762C54','Stickerbomb Hood')
+end)
